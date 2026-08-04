939b52afe4299211378c7d0c130205dcd031daa2
Arash <eshkevar.arash1387@gmail.com>
2026-08-04 22:27:10 +0330

    test func max()

--- NameSpace.py
+++ NameSpace.py
@@ -1,16 +1,14 @@
-import copy
-x = 1
-
-l = [1,2,3]
-s = 'arash'
-t = (1, 2, 3)
-sett = {1, 2, 3}
+l = [100, 20, 3, 80, 600]
 
 
-def func(a):
-    a += [4, 6]
-    print(a)
+def func_max(iterable):
+    mx = iterable[0]
+    for i in range(len(iterable) - 1):
+        if iterable[i + 1] > mx:
+            mx = iterable[i + 1]
 
-l_copy = copy.deepcopy(l)
-func(l_copy)
-print(l)
+    return mx
+
+
+mx = func_max(l)
+print(mx)
