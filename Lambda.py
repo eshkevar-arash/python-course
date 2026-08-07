7037a5bdcfeadcd994bec33e4ce89725045205d7
Arash <eshkevar.arash1387@gmail.com>
2026-08-07 09:07:11 +0330

    practic filter

--- Lambda.py
+++ Lambda.py
@@ -6,5 +6,9 @@ li = [1, 2, 3]
 # a = map(lambda x: x + 1, li)
 # print(list(a))
 
-li_2 = [item * 2 for item in li]
-print(li_2)
+# li_2 = [item * 2 for item in li]
+# print(li_2)
+# a = list(filter(lambda item: item % 2 == 0, li))
+# print(a)
+res = [item for item in li if item % 2 == 0]
+print(res)
