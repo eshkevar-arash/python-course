5859a6e495d1cba00e52379a1a72e7d3ab9c8f2d
Arash <eshkevar.arash1387@gmail.com>
2026-08-12 18:36:45 +0330

    practic arg

--- Decorator.py
+++ Decorator.py
@@ -1,15 +1,31 @@
-def say_hello():
-    print("hello")
+# def say_hello(name):
+#     print(f"Hello {name}")
+#
+#
+# def say_by():
+#     print("by")
+#
+#
+# def decorator(func):
+#     def wrapper(name):
+#         print("before")
+#         func(name)
+#         print("after")
+#
+#     return wrapper
+#
+#
+# # say_hello = decorator(say_hello)
+# say_hello("arash")
+
+# say_by = decorator(say_by)
+# say_by()
 
 
-def decorator(func):
+def func(*args):
+    print(type(args))
+    return args
 
-    def wrapper():
-        print("Before")
-        func()
-        print("After")
 
-    return wrapper
-
-x = decorator(say_hello)
-x()
+x = func(10, 20, 30, 40)
+print(x)
